f75ced61e50ef733308da37ac8fad4b72fccf9a3
muruk511 <tontanmotors@gmail.com>
2025-07-03 22:23:16 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+const fs = require('fs');
+
+const serviceAccount = JSON.parse(fs.readFileSync('/etc/secrets/firebase-service-account.json', 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
